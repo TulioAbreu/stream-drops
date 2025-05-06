efed5d9ae7704d56566572e55bbcbb56d4d415dc
TulioAbreu <marco-tulioabreu1@hotmail.com>
2025-05-05 23:03:58 -0300

    feat: add i18n support with Portuguese translations and language keys

---
+++ src/i18n/index.ts
@@ -0,0 +1,23 @@
+import { ptBRT } from "./pt-BR";
+
+// TODO: check a lib for this
+
+export type I18n = Record<
+    typeof I18NKeys[number],
+    string
+>;
+
+export const I18nLanguages = {
+    PT_BR: 'pt-BR',
+} as const;
+
+export type I18nLanguages = typeof I18nLanguages[keyof typeof I18nLanguages];
+
+export const I18NKeys = [
+    "LOGIN_TITLE",
+    "LOGIN_BUTTON_TWITCH",
+    "DASHBOARD_SIDEBAR_ITEM_FOLLOWER_GIVEAWAY",
+    "DASHBOARD_SIDEBAR_ITEM_TICKET_GIVEAWAY",
+] as const;
+
+export const T = ptBRT;
---
+++ src/i18n/pt-BR.ts
@@ -0,0 +1,8 @@
+import type { I18n } from ".";
+
+export const ptBRT: I18n = {
+    LOGIN_TITLE: "Login",
+    LOGIN_BUTTON_TWITCH: "Entrar com o Twitch",
+    DASHBOARD_SIDEBAR_ITEM_FOLLOWER_GIVEAWAY: "Sorteio para Seguidores",
+    DASHBOARD_SIDEBAR_ITEM_TICKET_GIVEAWAY: "Sorteio por Tickets",
+} as const;
