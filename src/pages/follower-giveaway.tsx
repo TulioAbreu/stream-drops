9fb4048c5ab1ab0e5341ef6108ce98fd49b8bb37
TulioAbreu <marco-tulioabreu1@hotmail.com>
2025-05-10 21:51:06 -0300

    feat: update FollowerGiveaway form to set default required subscriber to TIER_1 and improve layout structure

--- src/pages/follower-giveaway.tsx
+++ src/pages/follower-giveaway.tsx
@@ -24,9 +24,8 @@ export function FollowerGiveaway() {
         defaultValues: {
             title: "",
             description: "",
-            requiredSubscriber: SubscriberTier.FREE,
+            requiredSubscriber: SubscriberTier.TIER_1,
             subscriberMultiplier: {
-                [SubscriberTier.FREE]: 1,
                 [SubscriberTier.TIER_1]: 1,
                 [SubscriberTier.TIER_2]: 1,
                 [SubscriberTier.TIER_3]: 1,
@@ -36,81 +35,89 @@ export function FollowerGiveaway() {
 
     const requiredSubscriber = form.watch("requiredSubscriber");
 
+    const onClickSearchParticipants = () => {
+
+    };
+
     return (
         <Layout>
-            <div className="flex flex-col items-center justify-center flex-grow">
-                <Card className="rounded w-[600px]">
-                    <CardHeader>
-                        <CardTitle>{t("FOLLOWER_GIVEAWAY_TITLE")}</CardTitle>
-                        <CardDescription>{t("FOLLOWER_GIVEAWAY_DESCRIPTION")}</CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <Form {...form}>
-                            <form className="flex flex-col gap-4">
-                                <div className="flex flex-col gap-2">
-                                    <Label>{t("FOLLOWER_GIVEAWAY_FORM_TITLE_FIELD")}</Label>
-                                    <Input placeholder={t("FOLLOWER_GIVEAWAY_FORM_TITLE_FIELD")} />
-                                </div>
-                                <div className="flex flex-col gap-2">
-                                    <Label>{t("FOLLOWER_GIVEAWAY_FORM_DESCRIPTION_FIELD")}</Label>
-                                    <Textarea placeholder={t("FOLLOWER_GIVEAWAY_FORM_DESCRIPTION_FIELD")} className="resize-none" />
-                                </div>
-                                <div className="flex flex-col gap-2">
-                                    <Label>{t("FOLLOWER_GIVEAWAY_FORM_SUBSCRIPTION_REQUIREMENT_FIELD")}</Label>
-                                    <FormField
-                                        control={form.control}
-                                        name="requiredSubscriber"
-                                        render={({ field }) => (
-                                            <Select
-                                                onValueChange={(value) => {
-                                                    field.onChange(value);
-                                                }}
-                                            >
-                                                <SelectTrigger className="w-full">
-                                                    <SelectValue placeholder="Selecione" />
-                                                </SelectTrigger>
-                                                <SelectContent>
-                                                    {Object.values(SubscriberTier).map((tierValue) => (
-                                                        <SelectItem key={tierValue} value={String(tierValue)}>
-                                                            {SubscriberTierLabels[tierValue]}
-                                                        </SelectItem>
-                                                    ))}
-                                                </SelectContent>
-                                            </Select>
-                                        )}
-                                    />
-                                </div>
-                                <div className="flex flex-col gap-2">
-                                    <Label>Sorte de Sub</Label>
-                                    <Table>
-                                        <TableHeader>
-                                            <TableRow>
-                                                <TableHead>{t("FOLLOWER_GIVEAWAY_FORM_SUBSCRIPTION_TIER_TABLE_HEADER")}</TableHead>
-                                                <TableHead>{t("FOLLOWER_GIVEAWAY_FORM_SUBSCRIPTION_SUBSCRIBER_LUCK_TABLE_HEADER")}</TableHead>
-                                            </TableRow>
-                                        </TableHeader>
-                                        {Object.values(SubscriberTier).filter((tier) => tier >= requiredSubscriber).map((tierValue) => (
-                                            <TableRow key={tierValue}>
-                                                <TableCell>{SubscriberTierLabels[tierValue]}</TableCell>
-                                                <TableCell>
-                                                    <Input
-                                                        type="number"
-                                                        placeholder="0"
-                                                        className="w-[100px]"
-                                                        {...form.register(`subscriberMultiplier.${tierValue}` as any)}
-                                                    />
-                                                </TableCell>
-                                            </TableRow>
-                                        ))}
-                                    </Table>
-                                </div>
-                            </form>
-                        </Form>
-                    </CardContent>
-                    <CardFooter className="flex justify-end">
-                        <Button>Buscar Participantes</Button>
-                    </CardFooter>
-                </Card>
+            <div className="flex flex-row items-center justify-center flex-grow gap-4">
+                <div className="flex flex-row gap-4">
+                    <Card className="rounded w-[600px]">
+                        <CardHeader>
+                            <CardTitle>{t("FOLLOWER_GIVEAWAY_TITLE")}</CardTitle>
+                            <CardDescription>{t("FOLLOWER_GIVEAWAY_DESCRIPTION")}</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <Form {...form}>
+                                <form className="flex flex-col gap-4">
+                                    <div className="flex flex-col gap-2">
+                                        <Label>{t("FOLLOWER_GIVEAWAY_FORM_TITLE_FIELD")}</Label>
+                                        <Input placeholder={t("FOLLOWER_GIVEAWAY_FORM_TITLE_FIELD")} />
+                                    </div>
+                                    <div className="flex flex-col gap-2">
+                                        <Label>{t("FOLLOWER_GIVEAWAY_FORM_DESCRIPTION_FIELD")}</Label>
+                                        <Textarea placeholder={t("FOLLOWER_GIVEAWAY_FORM_DESCRIPTION_FIELD")} className="resize-none" />
+                                    </div>
+                                    <div className="flex flex-col gap-2">
+                                        <Label>{t("FOLLOWER_GIVEAWAY_FORM_SUBSCRIPTION_REQUIREMENT_FIELD")}</Label>
+                                        <FormField
+                                            control={form.control}
+                                            name="requiredSubscriber"
+                                            render={({ field }) => (
+                                                <Select
+                                                    onValueChange={(value) => {
+                                                        field.onChange(value);
+                                                    }}
+                                                >
+                                                    <SelectTrigger className="w-full">
+                                                        <SelectValue placeholder="Selecione" />
+                                                    </SelectTrigger>
+                                                    <SelectContent>
+                                                        {Object.values(SubscriberTier).map((tierValue) => (
+                                                            <SelectItem key={tierValue} value={String(tierValue)}>
+                                                                {SubscriberTierLabels[tierValue]}
+                                                            </SelectItem>
+                                                        ))}
+                                                    </SelectContent>
+                                                </Select>
+                                            )}
+                                        />
+                                    </div>
+                                    <div className="flex flex-col gap-2">
+                                        <Label>Sorte de Sub</Label>
+                                        <Table>
+                                            <TableHeader>
+                                                <TableRow>
+                                                    <TableHead>{t("FOLLOWER_GIVEAWAY_FORM_SUBSCRIPTION_TIER_TABLE_HEADER")}</TableHead>
+                                                    <TableHead>{t("FOLLOWER_GIVEAWAY_FORM_SUBSCRIPTION_SUBSCRIBER_LUCK_TABLE_HEADER")}</TableHead>
+                                                </TableRow>
+                                            </TableHeader>
+                                            {Object.values(SubscriberTier).filter((tier) => tier >= requiredSubscriber).map((tierValue) => (
+                                                <TableRow key={tierValue}>
+                                                    <TableCell>{SubscriberTierLabels[tierValue]}</TableCell>
+                                                    <TableCell>
+                                                        <Input
+                                                            type="number"
+                                                            placeholder="0"
+                                                            className="w-[100px]"
+                                                            {...form.register(`subscriberMultiplier.${tierValue}` as any)}
+                                                        />
+                                                    </TableCell>
+                                                </TableRow>
+                                            ))}
+                                        </Table>
+                                    </div>
+                                </form>
+                            </Form>
+                        </CardContent>
+                        <CardFooter className="flex justify-end">
+                            <Button>Buscar Participantes</Button>
+                        </CardFooter>
+                    </Card>
+                    <Card className="rounded w-[400px]">
+                    </Card>
+                </div>
             </div>
         </Layout>
     );
