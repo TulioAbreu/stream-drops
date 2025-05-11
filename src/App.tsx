cb4ef39e4d0a88d33b792e328b71746937325648
TulioAbreu <marco-tulioabreu1@hotmail.com>
2025-05-10 22:09:03 -0300

    feat: add route for login redirect page in the main application

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "./components/theme-provider"
 import { LoginPage } from "./pages/login"
 import { DashboardPage } from "./pages/dashboard"
 import { FollowerGiveaway } from "./pages/follower-giveaway"
+import { LoginRedirectPage } from "./pages/login-redirect"
 
 export function App() {
     return (
@@ -10,6 +11,7 @@ export function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<LoginPage />} />
+                    <Route path="/auth" element={<LoginRedirectPage />} />
                     <Route path="/dashboard" element={<DashboardPage />} />
                     <Route path="/dashboard/follower-giveaway" element={<FollowerGiveaway />} />
                 </Routes>
