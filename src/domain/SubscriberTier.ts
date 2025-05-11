5fb2e7856d1ca7d41eaa5345197a5ca8d020ceb1
TulioAbreu <marco-tulioabreu1@hotmail.com>
2025-05-10 21:10:18 -0300

    feat: remove FREE tier from SubscriberTier and update labels accordingly

--- src/domain/SubscriberTier.ts
+++ src/domain/SubscriberTier.ts
@@ -1,7 +1,6 @@
 export type SubscriberTier = typeof SubscriberTier[keyof typeof SubscriberTier];
 
 export const SubscriberTier = {
-    FREE: 0,
     TIER_1: 1000,
     TIER_2: 2000,
     TIER_3: 3000,
@@ -11,7 +10,6 @@ export const SubscriberTierLabels: Record<
     typeof SubscriberTier[keyof typeof SubscriberTier],
     string
 > = {
-    [SubscriberTier.FREE]: "Free",
     [SubscriberTier.TIER_1]: "Tier 1",
     [SubscriberTier.TIER_2]: "Tier 2",
     [SubscriberTier.TIER_3]: "Tier 3",
