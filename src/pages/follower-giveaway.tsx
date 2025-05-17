d52793e05c0f94f0148c3c1d535200aabf0bb1f7
TulioAbreu <marco-tulioabreu1@hotmail.com>
2025-05-17 01:25:28 -0300

    feat: integrate react-virtuoso for optimized participant table rendering in follower giveaway

--- src/pages/follower-giveaway.tsx
+++ src/pages/follower-giveaway.tsx
@@ -1,12 +1,13 @@
 import { Layout } from "@/components/layout";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { TableVirtuoso } from "react-virtuoso";
 import { Form, FormField } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Skeleton } from "@/components/ui/skeleton";
-import { Table, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Textarea } from "@/components/ui/textarea";
 import { SubscriberTier, SubscriberTierLabels } from "@/domain/SubscriberTier";
 import { useTwitchApi } from "@/hooks/use-twitch-api";
@@ -69,36 +70,50 @@ export function FollowerGiveaway() {
             };
             subscriptions.push(fakeSubscription);
         }
-
-        let nextPage = undefined;
-        do {
-            const response = await twitchApiClient.getBroadcasterSubscriptions({
-                broadcaster_id: userData?.id,
-                first: "100",
-                after: nextPage,
-            });
-            if (response.isErr()) {
-                console.error("Error fetching subscriptions:", response.error);
-                return;
-            } else {
-                subscriptions.push(...response.value.data);
-                nextPage = response.value.pagination.cursor;
-            }
-        } while (nextPage);
-        setIsLoadingUsers(false);
         setUsers(subscriptions);
+        setIsLoadingUsers(false);
+        // let nextPage = undefined;
+        // do {
+        //     const response = await twitchApiClient.getBroadcasterSubscriptions({
+        //         broadcaster_id: userData?.id,
+        //         first: "100",
+        //         after: nextPage,
+        //     });
+        //     if (response.isErr()) {
+        //         console.error("Error fetching subscriptions:", response.error);
+        //         return;
+        //     } else {
+        //         subscriptions.push(...response.value.data);
+        //         nextPage = response.value.pagination.cursor;
+        //     }
+        // } while (nextPage);
+        // setIsLoadingUsers(false);
+        // setUsers(subscriptions);
     };
 
     return (
         <Layout>
             <div className="flex flex-row justify-center flex-grow gap-4">
-                <div className="flex flex-col gap-4 w-full p-8 sm:flex-row">
-                    <Card className="rounded w-[60%] min-w-[400px]">
-                        <CardHeader>
-                            <CardTitle>{t("FOLLOWER_GIVEAWAY_TITLE")}</CardTitle>
-                            <CardDescription>{t("FOLLOWER_GIVEAWAY_DESCRIPTION")}</CardDescription>
-                        </CardHeader>
-                        <CardContent>
+                <div className="flex flex-col gap-4 w-full p-8">
+                    <div className="flex flex-row justify-between">
+                        <div>
+                            <h1 className="text-2xl font-bold">{t("FOLLOWER_GIVEAWAY_TITLE")}</h1>
+                            <p className="text-sm text-muted-foreground">{t("FOLLOWER_GIVEAWAY_DESCRIPTION")}</p>
+                        </div>
+                        <div className="flex flex-row gap-4">
+                            <Button type="button" onClick={onClickSearchParticipants}>{t("FOLLOWER_GIVEAWAY_FORM_FIND_PARTICIPANTS_BUTTON")}</Button>
+                            <Button
+                                variant="default"
+                                onClick={() => {}}
+                                className="min-w-[160px]"
+                                disabled={isLoadingUsers || users === null || users.length === 0}
+                            >
+                                Sortear
+                            </Button>
+                        </div>
+                    </div>
+                    <div className="flex flex-row gap-8 w-full">
+                        <div className="flex-1/3">
                             <Form {...form}>
                                 <form className="flex flex-col gap-4">
                                     <div className="flex flex-col gap-2">
@@ -160,48 +175,46 @@ export function FollowerGiveaway() {
                                     </div>
                                 </form>
                             </Form>
-                        </CardContent>
-                        <CardFooter className="flex justify-end">
-                            <Button onClick={onClickSearchParticipants}>{t("FOLLOWER_GIVEAWAY_FORM_FIND_PARTICIPANTS_BUTTON")}</Button>
-                        </CardFooter>
-                    </Card>
-                    {(isLoadingUsers || users) && (
-                        <Card className="rounded w-[40%] max-h-[400px] overflow-auto">
-                            <CardHeader>
-                                <CardTitle>{t("FOLLOWER_GIVEAWAY_FORM_PARTICIPANTS_TITLE")}</CardTitle>
-                            </CardHeader>
-                            <CardContent>
-                                {isLoadingUsers ? (
-                                    <div>
-
-                                    </div>
-                                ) : (
-                                    users && users.length === 0 ? (
-                                        <div className="flex flex-col items-center justify-center h-full">
+                        </div>
+                        <div className="flex flex-col gap-2 flex-1/3">
+                            <h2 className="text-lg font-bold">{t("FOLLOWER_GIVEAWAY_FORM_PARTICIPANTS_TITLE")}</h2>
+                            {isLoadingUsers ? (
+                                <Skeleton className="w-full h-full rounded-md" />
+                            ) : (
+                                <>
+                                    {(users === null || users.length === 0) && (
+                                        <div className="flex flex-col h-full">
                                             <p>{t("FOLLOWER_GIVEAWAY_FORM_NO_PARTICIPANTS")}</p>
                                         </div>
-                                    ) : (
-                                        users && (
-                                            <Table className="w-full">
-                                                <TableHeader>
+                                    )}
+                                    {users && users.length > 0 && (
+                                        <div className="flex flex-col h-full">
+                                            <TableVirtuoso
+                                                style={{ height: "100%" }}
+                                                data={users}
+                                                components={{
+                                                    Table,
+                                                    TableBody,
+                                                    TableRow,
+                                                    TableHead: TableHeader,
+                                                }}
+                                                fixedHeaderContent={() => (
                                                     <TableRow>
                                                         <TableHead>{t("FOLLOWER_GIVEAWAY_FORM_PARTICIPANTS_TABLE_HEADER")}</TableHead>
                                                         <TableHead>{t("FOLLOWER_GIVEAWAY_FORM_PARTICIPANTS_SUBSCRIPTION_TIER_TABLE_HEADER")}</TableHead>
                                                     </TableRow>
-                                                </TableHeader>
-                                                {users.map((user) => (
-                                                    <TableRow key={user.user_id}>
-                                                        <TableCell>{user.user_name}</TableCell>
-                                                        <TableCell>{user.tier}</TableCell>
-                                                    </TableRow>
-                                                ))}
-                                            </Table>
-                                        )
-                                    )
-                                )}
-                            </CardContent>
-                        </Card>
-                    )}
+                                                )}
+                                                itemContent={(_index, user) => [
+                                                    <TableCell>{user.user_name}</TableCell>,
+                                                    <TableCell>{user.tier}</TableCell>
+                                                ]}
+                                            />
+                                        </div>
+                                    )}
+                                </>
+                            )}
+                        </div>
+                    </div>
                 </div>
             </div>
         </Layout>
