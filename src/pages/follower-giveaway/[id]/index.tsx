3ae786b8902c4ce357d870ae1ba1ad5b46a220a5
TulioAbreu <marco-tulioabreu1@hotmail.com>
2025-05-17 02:00:15 -0300

    feat: refactor follower giveaway form and integrate participant search functionality

---
+++ src/pages/follower-giveaway/[id]/index.tsx
@@ -0,0 +1,107 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { useTwitchApi } from "@/hooks/use-twitch-api";
+import type { BroadcasterSubscriber } from "@/service/twitch/types";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useParams } from "react-router";
+import { TableVirtuoso } from "react-virtuoso";
+
+export function FollowerGiveawayId() {
+    const { id } = useParams<{ id: string }>();
+    const { t } = useTranslation();
+    const { twitchApiClient, userData } = useTwitchApi();
+    const [users, setUsers] = useState<BroadcasterSubscriber[] | null>(null);
+    const [isLoadingUsers, setIsLoadingUsers] = useState<boolean>(false);
+
+
+    const onClickSearchParticipants = async () => {
+        if (!twitchApiClient || !userData) {
+            return;
+        }
+
+        setIsLoadingUsers(true);
+        const subscriptions: BroadcasterSubscriber[] = [];
+
+        const tiers = ["1000", "2000", "3000"] as const;
+        for (let i = 0; i < 10000; i++) {
+            const randomTier = tiers[Math.floor(Math.random() * tiers.length)];
+            const fakeSubscription: BroadcasterSubscriber = {
+                user_id: `user_${i}`,
+                user_name: `user_${i}`,
+                tier: randomTier,
+                is_gift: false,
+                broadcaster_id: userData.id,
+                broadcaster_name: userData.login,
+                broadcaster_login: userData.login,
+                gifter_id: "",
+                gifter_login: "",
+                plan_name: "",
+                user_login: `user_${i}`,
+            };
+            subscriptions.push(fakeSubscription);
+        }
+        setUsers(subscriptions);
+        setIsLoadingUsers(false);
+        // let nextPage = undefined;
+        // do {
+        //     const response = await twitchApiClient.getBroadcasterSubscriptions({
+        //         broadcaster_id: userData?.id,
+        //         first: "100",
+        //         after: nextPage,
+        //     });
+        //     if (response.isErr()) {
+        //         console.error("Error fetching subscriptions:", response.error);
+        //         return;
+        //     } else {
+        //         subscriptions.push(...response.value.data);
+        //         nextPage = response.value.pagination.cursor;
+        //     }
+        // } while (nextPage);
+        // setIsLoadingUsers(false);
+        // setUsers(subscriptions);
+    };
+
+    return (
+        <div className="flex flex-row gap-8 w-full">
+            <div className="flex flex-col gap-2 flex-1/3">
+                <h2 className="text-lg font-bold">{t("FOLLOWER_GIVEAWAY_FORM_PARTICIPANTS_TITLE")}</h2>
+                {isLoadingUsers ? (
+                    <Skeleton className="w-full h-full rounded-md" />
+                ) : (
+                    <>
+                        {(users === null || users.length === 0) && (
+                            <div className="flex flex-col h-full">
+                                <p>{t("FOLLOWER_GIVEAWAY_FORM_NO_PARTICIPANTS")}</p>
+                            </div>
+                        )}
+                        {users && users.length > 0 && (
+                            <div className="flex flex-col h-full">
+                                <TableVirtuoso
+                                    style={{ height: "100%" }}
+                                    data={users}
+                                    components={{
+                                        Table,
+                                        TableBody,
+                                        TableRow,
+                                        TableHead: TableHeader,
+                                    }}
+                                    fixedHeaderContent={() => (
+                                        <TableRow>
+                                            <TableHead>{t("FOLLOWER_GIVEAWAY_FORM_PARTICIPANTS_TABLE_HEADER")}</TableHead>
+                                            <TableHead>{t("FOLLOWER_GIVEAWAY_FORM_PARTICIPANTS_SUBSCRIPTION_TIER_TABLE_HEADER")}</TableHead>
+                                        </TableRow>
+                                    )}
+                                    itemContent={(_index, user) => [
+                                        <TableCell>{user.user_name}</TableCell>,
+                                        <TableCell>{user.tier}</TableCell>
+                                    ]}
+                                />
+                            </div>
+                        )}
+                    </>
+                )}
+            </div>
+        </div>
+    );
+}
--- src/pages/follower-giveaway/index.tsx
+++ src/pages/follower-giveaway/index.tsx
@@ -1,125 +1,141 @@
-import { Layout } from "@/components/layout";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { TableVirtuoso } from "react-virtuoso";
 import { Form, FormField } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Skeleton } from "@/components/ui/skeleton";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Table, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Textarea } from "@/components/ui/textarea";
 import { SubscriberTier, SubscriberTierLabels } from "@/domain/SubscriberTier";
-import { useTwitchApi } from "@/hooks/use-twitch-api";
-import { useTranslation } from "@/i18n";
-import type { BroadcasterSubscriber } from "@/service/twitch/types";
-import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { FollowerGiveawayForm } from "./components/follower-giveaway-form";
+import { type FollowerGiveawayForm } from "./types";
+import { useTranslation } from "react-i18next";
+import { Button } from "@/components/ui/button";
+import { v7 } from "uuid";
+import { useSubscriptionGiveawayDb } from "@/database";
+import { Layout } from "@/components/layout";
 
-export function FollowerGiveaway() {
+interface FollowerGiveawayFormProps {
+    onSubmit: (data: FollowerGiveawayForm) => void;
+}
+
+export function FollowerGiveaway({ onSubmit }: FollowerGiveawayFormProps) {
     const { t } = useTranslation();
-    const { twitchApiClient, userData } = useTwitchApi();
-    const [users, setUsers] = useState<BroadcasterSubscriber[] | null>(null);
-    const [isLoadingUsers, setIsLoadingUsers] = useState<boolean>(false);
+    const { addGiveaway } = useSubscriptionGiveawayDb();
+    const form = useForm<FollowerGiveawayForm>({
+        defaultValues: {
+            title: "",
+            description: "",
+            requiredSubscriber: SubscriberTier.TIER_1,
+            subscriberMultiplier: {
+                [SubscriberTier.TIER_1]: 1,
+                [SubscriberTier.TIER_2]: 1,
+                [SubscriberTier.TIER_3]: 1,
+            },
+        },
+    });
 
-
-    const onClickSearchParticipants = async () => {
-        if (!twitchApiClient || !userData) {
-            return;
-        }
-
-        setIsLoadingUsers(true);
-        const subscriptions: BroadcasterSubscriber[] = [];
-
-        const tiers = ["1000", "2000", "3000"] as const;
-        for (let i = 0; i < 10000; i++) {
-            const randomTier = tiers[Math.floor(Math.random() * tiers.length)];
-            const fakeSubscription: BroadcasterSubscriber = {
-                user_id: `user_${i}`,
-                user_name: `user_${i}`,
-                tier: randomTier,
-                is_gift: false,
-                broadcaster_id: userData.id,
-                broadcaster_name: userData.login,
-                broadcaster_login: userData.login,
-                gifter_id: "",
-                gifter_login: "",
-                plan_name: "",
-                user_login: `user_${i}`,
-            };
-            subscriptions.push(fakeSubscription);
-        }
-        setUsers(subscriptions);
-        setIsLoadingUsers(false);
-        // let nextPage = undefined;
-        // do {
-        //     const response = await twitchApiClient.getBroadcasterSubscriptions({
-        //         broadcaster_id: userData?.id,
-        //         first: "100",
-        //         after: nextPage,
-        //     });
-        //     if (response.isErr()) {
-        //         console.error("Error fetching subscriptions:", response.error);
-        //         return;
-        //     } else {
-        //         subscriptions.push(...response.value.data);
-        //         nextPage = response.value.pagination.cursor;
-        //     }
-        // } while (nextPage);
-        // setIsLoadingUsers(false);
-        // setUsers(subscriptions);
+    const requiredSubscriber = form.watch("requiredSubscriber");
+    
+    const onClickSubmit = (data: FollowerGiveawayForm) => {
+        addGiveaway({
+            ...data,
+            id: v7(),
+            description: data.description,
+            requiredSubscriber: data.requiredSubscriber,
+            subscriberMultiplier: data.subscriberMultiplier,
+            title: data.title,
+            participants: [],
+            winners: [],
+        })
+            .then(() => {
+                onSubmit(data);
+        })
+            .then(() => {
+                alert("Giveaway added successfully");
+            })
     };
 
     return (
         <Layout>
-            <div className="flex flex-row justify-center flex-grow gap-4">
-                <div className="flex flex-col gap-4 w-full p-8">
-                        <FollowerGiveawayForm
-                            onSubmit={onClickSearchParticipants}
-                        />
-                    <div className="flex flex-row gap-8 w-full">
-                        <div className="flex flex-col gap-2 flex-1/3">
-                            <h2 className="text-lg font-bold">{t("FOLLOWER_GIVEAWAY_FORM_PARTICIPANTS_TITLE")}</h2>
-                            {isLoadingUsers ? (
-                                <Skeleton className="w-full h-full rounded-md" />
-                            ) : (
-                                <>
-                                    {(users === null || users.length === 0) && (
-                                        <div className="flex flex-col h-full">
-                                            <p>{t("FOLLOWER_GIVEAWAY_FORM_NO_PARTICIPANTS")}</p>
-                                        </div>
-                                    )}
-                                    {users && users.length > 0 && (
-                                        <div className="flex flex-col h-full">
-                                            <TableVirtuoso
-                                                style={{ height: "100%" }}
-                                                data={users}
-                                                components={{
-                                                    Table,
-                                                    TableBody,
-                                                    TableRow,
-                                                    TableHead: TableHeader,
-                                                }}
-                                                fixedHeaderContent={() => (
-                                                    <TableRow>
-                                                        <TableHead>{t("FOLLOWER_GIVEAWAY_FORM_PARTICIPANTS_TABLE_HEADER")}</TableHead>
-                                                        <TableHead>{t("FOLLOWER_GIVEAWAY_FORM_PARTICIPANTS_SUBSCRIPTION_TIER_TABLE_HEADER")}</TableHead>
-                                                    </TableRow>
-                                                )}
-                                                itemContent={(_index, user) => [
-                                                    <TableCell>{user.user_name}</TableCell>,
-                                                    <TableCell>{user.tier}</TableCell>
-                                                ]}
-                                            />
-                                        </div>
-                                    )}
-                                </>
-                            )}
-                        </div>
-                    </div>
-                </div>
+            <div className="flex flex-col">
+                <h1 className="text-2xl font-bold">{t("FOLLOWER_GIVEAWAY_TITLE")}</h1>
             </div>
+            <Form {...form}>
+                <form className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-2">
+                        <Label>{t("FOLLOWER_GIVEAWAY_FORM_TITLE_FIELD")}</Label>
+                        <Input
+                            placeholder={t("FOLLOWER_GIVEAWAY_FORM_TITLE_FIELD")}
+                            {...form.register("title", {
+                                required: t("FOLLOWER_GIVEAWAY_FORM_TITLE_FIELD_REQUIRED"),
+                            })}
+                        />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <Label>{t("FOLLOWER_GIVEAWAY_FORM_DESCRIPTION_FIELD")}</Label>
+                        <Textarea
+                            placeholder={t("FOLLOWER_GIVEAWAY_FORM_DESCRIPTION_FIELD")}
+                            className="resize-none"
+                            {...form.register("description", {
+                                required: t("FOLLOWER_GIVEAWAY_FORM_DESCRIPTION_FIELD_REQUIRED"),
+                            })}
+                        />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <Label>{t("FOLLOWER_GIVEAWAY_FORM_SUBSCRIPTION_REQUIREMENT_FIELD")}</Label>
+                        <FormField
+                            control={form.control}
+                            name="requiredSubscriber"
+                            render={({ field }) => (
+                                <Select
+                                    onValueChange={(value) => {
+                                        field.onChange(value);
+                                    }}
+                                >
+                                    <SelectTrigger className="w-full">
+                                        <SelectValue placeholder="Selecione" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        {Object.values(SubscriberTier).map((tierValue) => (
+                                            <SelectItem key={tierValue} value={String(tierValue)}>
+                                                {SubscriberTierLabels[tierValue]}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectContent>
+                                </Select>
+                            )}
+                        />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <Label>{t("FOLLOWER_GIVEAWAY_FORM_SUBSCRIBER_LUCK")}</Label>
+                        <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead>{t("FOLLOWER_GIVEAWAY_FORM_SUBSCRIPTION_TIER_TABLE_HEADER")}</TableHead>
+                                    <TableHead>{t("FOLLOWER_GIVEAWAY_FORM_SUBSCRIPTION_SUBSCRIBER_LUCK_TABLE_HEADER")}</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            {Object.values(SubscriberTier).filter((tier) => tier >= requiredSubscriber).map((tierValue) => (
+                                <TableRow key={tierValue}>
+                                    <TableCell>{SubscriberTierLabels[tierValue]}</TableCell>
+                                    <TableCell>
+                                        <Input
+                                            type="number"
+                                            placeholder="0"
+                                            className="w-[100px]"
+                                            {...form.register(`subscriberMultiplier.${tierValue}` as any)}
+                                        />
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </Table>
+                    </div>
+                    <div className="flex flex-row items-center justify-end gap-2">
+                        <Button type="submit" onClick={form.handleSubmit(onClickSubmit)}>
+                            {t("FOLLOWER_GIVEAWAY_FORM_FIND_PARTICIPANTS_BUTTON")}
+                        </Button>
+                    </div>
+                </form>
+            </Form>
         </Layout>
-    );
+    )
 }
