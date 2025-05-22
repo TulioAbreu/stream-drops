bb8241da29512b9a5bbae75c25ac868cd8227351
TulioAbreu <marco-tulioabreu1@hotmail.com>
2025-05-21 21:33:17 -0300

    refactor: remove Google Drive authentication code and clean up login logic

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -7,7 +7,7 @@ import { useTranslation } from "@/i18n";
 import { TWITCH_CLIENT_ID } from "@/settings";
 import { useLoginStore } from "@/storage/login";
 import { AvatarImage } from "@radix-ui/react-avatar";
-import { CheckIcon, CloudIcon, Loader } from "lucide-react";
+import { CheckIcon } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 
@@ -17,28 +17,14 @@ AUTHORIZATION_URL.searchParams.set("redirect_uri", "http://localhost:3000/auth")
 AUTHORIZATION_URL.searchParams.set("response_type", "token");
 AUTHORIZATION_URL.searchParams.set("scope", "channel:read:subscriptions");
 
-const AUTHORIZATION_DRIVE_URL = new URL("/o/oauth2/v2/auth", "https://accounts.google.com");
-AUTHORIZATION_DRIVE_URL.searchParams.set("client_id", "790178845295-d80705l73fje56tomu29lnmlspl85lnt.apps.googleusercontent.com");
-AUTHORIZATION_DRIVE_URL.searchParams.set("redirect_uri", "http://localhost:3000/auth/drive");
-AUTHORIZATION_DRIVE_URL.searchParams.set("response_type", "code");
-const SCOPES = [
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/spreadsheets",
-]
-AUTHORIZATION_DRIVE_URL.searchParams.set("scope", SCOPES.join(" "));
-AUTHORIZATION_DRIVE_URL.searchParams.set("access_type", "offline");
-AUTHORIZATION_DRIVE_URL.searchParams.set("prompt", "consent");
-
 export function LoginPage() {
     const { t } = useTranslation();
     const navigate = useNavigate();
-    const { setTwitchAccessToken, setDriveCode, driveCode, twitchAccessToken } = useLoginStore();
+    const { setTwitchAccessToken, twitchAccessToken } = useLoginStore();
     const { userData } = useTwitchApi();
     const [isLoadingTwitch, setIsLoadingTwitch] = useState<boolean>(false);
-    const [isLoadingDrive, _setIsLoadingDrive] = useState<boolean>(false);
 
     const handleLoginTwitch = () => {
-        // TODO: Implement timeout for the popup
         setIsLoadingTwitch(true);
         window.open(AUTHORIZATION_URL, "newwindow", "width=400,height=600,status=no,toolbar=no,menubar=no,location=no");
         window.addEventListener("message", (event) => {
@@ -50,17 +36,6 @@ export function LoginPage() {
         });
     };
 
-    const handleLoginGoogleDrive = () => {
-        window.open(AUTHORIZATION_DRIVE_URL, "newwindow", "width=400,height=600,status=no,toolbar=no,menubar=no,location=no");
-        window.addEventListener("message", (event) => {
-            if (event.data.type !== "drive-auth") {
-                return;
-            }
-            const code = event.data.code;
-            setDriveCode(code);
-        });
-    };
-
     useEffect(() => {
         if (userData) {
             setIsLoadingTwitch(false);
@@ -68,13 +43,13 @@ export function LoginPage() {
     }, [userData]);
 
     useEffect(() => {
-        if (!driveCode || !twitchAccessToken) {
+        if (!twitchAccessToken) {
             return;
         }
         setTimeout(() => {
             navigate("/dashboard");
         }, 1000);
-    }, [driveCode, twitchAccessToken]);
+    }, [twitchAccessToken]);
 
     return (
         <div className="flex flex-col items-center justify-center h-screen">
@@ -114,26 +89,7 @@ export function LoginPage() {
                             </Button>
                         )
                     )}
-                    {driveCode ? (
-                        <div className="flex flex-row items-center p-4 border rounded shadow-md gap-4">
-                            {isLoadingDrive ? (
-                                <>
-                                    <Loader className="w-5 h-5 animate-spin text-blue-500" />
-                                    <p>Autenticando Google Drive...</p>
-                                </>
-                            ) : (
-                                <>
-                                    <CloudIcon className="w-5 h-5" />
-                                    <p>Google Drive autenticado.</p>
-                                </>
-                            )}
-                        </div>
-                    ) : (
-                        <Button variant="default" className="w-full mt-4" onClick={handleLoginGoogleDrive}>
-                            {t("LOGIN_BUTTON_GOOGLE_DRIVE")}
-                        </Button>
-                    )}
-                    {(driveCode && twitchAccessToken) && (
+                    {twitchAccessToken && (
                         <div className="flex flex-row items-center p-4 border rounded shadow-md gap-4">
                             <CheckIcon className="w-5 h-5 text-green-500" />
                             <p>Tudo pronto, finalizando login...</p>
