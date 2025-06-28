06a9ce1942aac2ab04720dd8148aab0ac067a331
TulioAbreu <marco-tulioabreu1@hotmail.com>
2025-06-28 19:22:42 -0300

    refactor: extract TwitchUser type for improved clarity in GetTwitchUsersResponse

--- src/service/twitch/types.ts
+++ src/service/twitch/types.ts
@@ -39,17 +39,19 @@ export type GetTwitchUsersParams = {
 }
 
 export type GetTwitchUsersResponse = {
-    data: {
-        id: string;
-        login: string;
-        display_name: string;
-        type: string;
-        broadcaster_type: string;
-        description: string;
-        profile_image_url: string;
-        offline_image_url: string;
-        view_count: number;
-        email: string;
-        created_at: string;
-    }[]
+    data: TwitchUser[];
 }
+
+export type TwitchUser = {
+    id: string;
+    login: string;
+    display_name: string;
+    type: string;
+    broadcaster_type: string;
+    description: string;
+    profile_image_url: string;
+    offline_image_url: string;
+    view_count: number;
+    email: string;
+    created_at: string;
+};
