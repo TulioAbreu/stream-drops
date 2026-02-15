0af574b46c555326921e7d90207347a906b99918
TulioAbreu <marco-tulioabreu1@hotmail.com>
2026-02-15 00:52:38 -0300

    feat: Implement sending Twitch chat messages to announce giveaway winners with their win chance and tickets.

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -14,7 +14,7 @@ const AUTHORIZATION_URL = new URL("/oauth2/authorize", "https://id.twitch.tv");
 AUTHORIZATION_URL.searchParams.set("client_id", import.meta.env.VITE_TWITCH_CLIENT_ID);
 AUTHORIZATION_URL.searchParams.set("redirect_uri", import.meta.env.VITE_TWITCH_REDIRECT_URL);
 AUTHORIZATION_URL.searchParams.set("response_type", "token");
-AUTHORIZATION_URL.searchParams.set("scope", "channel:read:subscriptions");
+AUTHORIZATION_URL.searchParams.set("scope", "channel:read:subscriptions user:read:chat user:write:chat");
 
 export function LoginPage() {
     const { t } = useTranslation();
